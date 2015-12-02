48eb28b738bc538bb658495607135de2b19d6825
zhangchunsheng <zhangchunsheng423@gmail.com>
2015-12-02 18:22:07 +0800

    1.新功能:新增签到功能

--- install/install.sql
+++ install/install.sql
@@ -21,6 +21,7 @@ create table activity_members(
     activity_member_id int(11) not null auto_increment,
     activity_id int(11) not null default 0 comment '活动ID',
     member_name varchar(60) not null default '' comment '成员名称',
+    cellphone varchar(32) not null default '' comment '手机号',
     create_time int(11) not null default 0 comment '创建时间',
     update_time int(11) not null default 0 comment '更新时间',
     status int(11) not null default 0 comment '1 - 正常 2 － 删除',
@@ -41,6 +42,8 @@ create table activity_check_in_log(
 insert into activity(activity_name,activity_description,member_num,round_num,round_check_num,create_time,status)
 values
 ('2015年网站组团建-滑雪','2015年网站组团建-滑雪',11,1,0,unix_timestamp(now()),1);
+
+alter table activity_members add column cellphone varchar(32) not null default '' comment '手机号';
 
 insert into activity_members(activity_id,member_name,create_time,status)
 values
@@ -89,3 +92,13 @@ values
 insert into activity_check_in_log(activity_id,round_num,activity_member_id,check_time)
 values
 (1,1,1,unix_timestamp(now()));
+
+update activity_members set cellphone='13439174818' where activity_member_id=1;
+update activity_members set cellphone='13911168500' where activity_member_id=2;
+update activity_members set cellphone='15650727463' where activity_member_id=4;
+update activity_members set cellphone='13521718910' where activity_member_id=5;
+update activity_members set cellphone='13810664119' where activity_member_id=6;
+update activity_members set cellphone='18518407512' where activity_member_id=8;
+update activity_members set cellphone='15010195133' where activity_member_id=9;
+update activity_members set cellphone='15210365263' where activity_member_id=10;
+update activity_members set cellphone='18210189805' where activity_member_id=11;
